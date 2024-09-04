e5c4a8614a38e1154340cb4499ce219ed3258b6d
Jakub Kowalczyk <jakub8.kowalczyk@student.uj.edu.pl>
2024-09-04 20:18:31 +0200

    better ordering tests

--- tests/algorithms/ordering_tests.py
+++ tests/algorithms/ordering_tests.py
@@ -21,10 +21,10 @@ class Test(TestCase):
         graph.add_edge(7, 4)
         graph.add_edge(7, 5)
 
-        s = 0
+        s = 7
         dfs_plus_tiebreaking_order = [i for i in range(n)]
         order = dfs_plus(graph, dfs_plus_tiebreaking_order)
         tree = last_in_tree(graph, order)
-        result = ordering(graph, tree, s, dfs_plus_tiebreaking_order[::-1])
+        result = ordering(graph, tree, s, dfs_plus_tiebreaking_order)
 
-        self.assertEqual(result, [0, 1, 7, 4, 6, 2, 5, 3])
+        self.assertEqual(result, order)
